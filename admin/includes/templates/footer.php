c6727972cd3fdc6b9ded04dd9c2b08b1c3b82b03
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-19 21:26:34 +0100

    project's structure with header and footer

---
+++ admin/includes/templates/footer.php
@@ -0,0 +1,4 @@
+
+        <h1>Test the header and footer include method</h1>
+    </body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Include The Header From Template Include
+    include "./includes/templates/header.php";
+
+?>
+
+
+<?php
+
+    // Include The Footer From Template Include
+    include "./includes/templates/footer.php";
+
+?>
