5b02185ebc10edfd81c608b0e37afdfc00703342
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-05 23:31:04 +0100

    language key words for home page has been added

--- admin/includes/languages/english.php
+++ admin/includes/languages/english.php
@@ -5,6 +5,12 @@
     function lang($phrase){
 
         static $languages = array(
+            "HOME" => "Home",
+            "PRODUCT" => "Product",
+            "CATEGORIES" => "Categories",
+            "CAT1" => "Fashon",
+            "CAT2" => "Toys",
+
             "GREETING" => "Welcome",
             "ADMIN" => "administrator"
         );
--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -7,24 +7,21 @@
   <div class="collapse navbar-collapse" id="zakaria">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><?php echo lang("HOME") ?> </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#"><?php echo lang("PRODUCT") ?></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          <?php echo lang("CATEGORIES") ?>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#"><?php echo lang("CAT1") ?></a>
+          <a class="dropdown-item" href="#"><?php echo lang("CAT2") ?></a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
   </div>
