a4aeb68d7f9f2a14859012d13df8aafcc7c53edb
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-06 00:12:43 +0100

    some tricks

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -1,28 +1,39 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#zakaria" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#zakaria" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="zakaria">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><?php echo lang("HOME") ?> </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><?php echo lang("PRODUCT") ?></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <?php echo lang("CATEGORIES") ?>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><?php echo lang("CAT1") ?></a>
-          <a class="dropdown-item" href="#"><?php echo lang("CAT2") ?></a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
+    <div class="collapse navbar-collapse" id="zakaria">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><?php echo lang("HOME") ?> </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><?php echo lang("PRODUCT") ?></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><?php echo lang("PRODUCT") ?></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><?php echo lang("PRODUCT") ?></a>
+        </li>
+        
+      </ul>
+      <ul class="nav justify-contnet-end">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Zakaria
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Edit Profile</a>
+            <a class="dropdown-item" href="#">Settings</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Logout</a>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
