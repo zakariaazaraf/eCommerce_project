550a822a8b554f75eed4a3b3fa34a0a3b68c7b96
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-07 13:07:05 +0100

    update section and update database infos

--- admin/members.php
+++ admin/members.php
@@ -11,6 +11,8 @@
     //START THE SESSION, ALWAYS REMEMBER TO START IT
     session_start();
 
+    $titlePage = 'Members';
+
 
     // CHECK IF THE USER HAVE THE ACCESS TO THIS PAGE
     if(isset($_SESSION['username'])){
@@ -25,16 +27,91 @@
             $do = 'Manage';
         }
 
+
         if($do == 'Manage'){
+
             echo 'Manage page';
-        }elseif ($do == 'Edit'){
-            echo "Edit page";
-            echo "Your Id = " . $_GET['userid'];
+
+        }elseif ($do == 'Edit'){ 
+
+        
+        $userid = isset($_GET['userid']) && is_numeric($_GET['userid']) ? intval($_GET['userid'])  : 0;
+
+        // GET THE INFORMATION OF THIS USER BY HIS ID
+        $stmt = $db->prepare("SELECT * FROM users WHERE userId = ? LIMIT 1"); // QUERY
+        $stmt->execute(array($userid)); // PASS THE PARAMETER AND EXECUTE THE QUEY
+        $data = $stmt->fetch(); // FETCH THE DATA 
+        $rowCount = $stmt->rowCount(); // GET THE NUMBER OF ROWS
+
+        /* echo "Number of Rows" . $rowCount;
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>"; */
+
+            if($rowCount > 0){
+
+                ?> 
+
+
+                    <div class="form-container">
+                    <h1>edit member</h1>
+                        <form action="?do=update" method='POST'>
+                            <!-- HIDDEN INPUT FIELD TO SEND userid -->
+                            <input type="hidden" name="userid" value="<?php echo $userid?>">
+                            <div class="group">
+                                <label for="username">Username:</label>
+                                <input type="text" name="username" id="username"  value="<?php echo $data["UserName"]?>" autocomplete="off">
+                            </div>
+                            <div class="group">
+                                <label for="password">Password:</label>
+                                <input type="text" name="password" id="password" value="<?php echo $data["Password"] ?>" autocomplete="new-password">
+                            </div>
+                            <div class="group">
+                                <label for="email">Email:</label>
+                                <input type="email" name="email" id="email" value="<?php echo $data["Email"]?>">
+                            </div>
+                            <div class="group">
+                                <label for="fullname">Full Name:</label>
+                                <input type="text" name="fullname" id="fullname" value="<?php echo $data["FullName"]?>"/>
+                            </div>
+
+                            <input type="submit" value="save" class="btn btn-primary">
+                        </form>
+                    </div>    
+
+            <?php } 
+
+            else{
+            echo "there's no such ID";
+            }
+      
+    }elseif($do == 'update'){
+        
+        // CHECK IF THE USER CAME BY A POST REQUEST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            
+            // GET THE DATA FROM THE POST REQUEST
+            $id = $_POST['userid'];
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $fullname = $_POST['fullname'];
+
+            // UPDATE THE IFORMATION BY SEND A QUERY TO DATABASE
+            $stmt = $db->prepare("UPDATE users SET UserName = ?, Email = ?, FullName = ? WHERE userId = ?");
+            $stmt->execute(array($username, $email, $fullname, $id)); // PASS THE PARAMS AND EXECUTE THE QUERY
+
+            $row = $stmt->rowCount();
+
+            echo $row . " of records updated !!" ;
+
+
         }else{
-            echo 'default';
+            echo "You Cant't Access This Page Directly ";
         }
 
-
+    }else{
+            echo 'default';
+        }
         //Include Footer
         include $template . 'footer.php';
 
