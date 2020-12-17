5e4fd8856144e38c06e863264c9d25161b19bd2b
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-17 23:58:31 +0100

    use the checkItem function to get the panding members

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,7 +26,7 @@
     <div class="container home-static text-center text-capitalize">
         <div class="row">
             <div class="col-md-3"><div class='static st-members'>total members<a href="members.php"><span class='d-block'><?php echo countColumns('UserId', 'users')?></span></a></div></div>
-            <div class="col-md-3"><div class='static st-panding'>panding memebers<a href='members.php?do=Manage&page=panding'><span class='d-block'>22</span></a></div></div>
+            <div class="col-md-3"><div class='static st-panding'>panding memebers<a href='members.php?do=Manage&page=panding'><span class='d-block'><?php echo checkItem('RegStatus', 'users', 0)?></span></a></div></div>
             <div class="col-md-3"><div class='static st-items'>total items<span class='d-block'>1500</span></div></div>
             <div class="col-md-3"><div class='static st-comments'>total comments<span class='d-block'>3300</span></div></div>
         </div>
