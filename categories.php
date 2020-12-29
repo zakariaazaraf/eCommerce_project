b20ccccb59b29989bcc9bb5ae4b9bce783937835
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-29 18:46:58 +0100

    add the price feature

--- categories.php
+++ categories.php
@@ -16,6 +16,7 @@
                         echo "<div class='card-title'>".$item['Name']."</div>";
                         echo "<div class='card-text'>".$item['Description']."</div>";
                         echo "<div class='card-text'><small class='text-muted'>".$item['Made_In']."</small></div>";
+                        echo "<span class='price'>".$item['Price']."</sapn>";
                     echo "</div>";
                 echo "</div>";
             }
